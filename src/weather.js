e4fd86223ff51436ae6d53b379f3e2a5a7f11a73
renpond <101608612+renpond@users.noreply.github.com>
2022-08-23 15:24:57 -0500

    Created Weather-React

---
+++ src/weather.js
@@ -0,0 +1,18 @@
+import React from "react";
+import axios from "axios";
+import * as Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} 
+      is ${response.data.main.temp}°F`
+    );
+  }
+  let apiKey = "54cb345a2c3729ba77c24984961b3eee";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/
+  weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <Loader.InfinitySpin />;
+}
